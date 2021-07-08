80f363ef489eabfa15d4d31c5443b72709ef6d85
davidalanfrank <me@example.com>
2021-07-08 12:56:48 +1000

    Adding some more content into billboard details

--- src/pages/Billboard.jsx
+++ src/pages/Billboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./billboard.scss";
+import SyntaxHighLighter from "react-syntax-highlighter";
 
 export default function Billboard() {
   return (
@@ -62,18 +63,17 @@ export default function Billboard() {
             The figure below outlines the network architecure at a high-level.
             <br></br>
             <br></br>
-            <img alt="hl network architecture diagram"></img>
+            <img
+              alt="hl network architecture diagram"
+              src="../assets/hl-bb.png "
+              className="hl-bb"
+            ></img>
             <br></br>
             <br></br>
             Model-View-Controller is architectural design pattern used in this
             project. In this pattern, the user can make changes to the model via
             the controller. The model acts as a "single source of truth" that
-            updates the viewer which the user can see. Think of a painter in
-            their studio who paints using the individual colours on their
-            pallate (the model). This painter has a concept or an idea that will
-            govern how they use the paints on the canvas and how these will be
-            executed (the controller). These ideas are then painted on to the
-            cavnas for both the painter and other people to see (the viewer).
+            updates the viewer which the user can see.
             <br></br>
             <br></br>
             MVC provided the team with 3 main benefits:
@@ -98,6 +98,66 @@ export default function Billboard() {
             discuss my expierence using Driven Develpment (TDD), our use of
             mocking and finally Tracer-bullets.
             <h2>Test-driven development</h2>
+            <SyntaxHighLighter language="java">
+              {`public class Billboard {
+
+                  private String createdBy;
+                  private String message;
+                  private String information;
+                  private String picture;
+                  private String bgColour;
+                  private String messageColour;
+                  private String informationColour;
+                  private String name;
+
+            `}
+            </SyntaxHighLighter>
+            TDD is software development practice that puts emphasis on the
+            definition of the functional utility of a method first, rather than
+            the method's mechanics. This is done by programming the functional
+            test before any code has been built. The advantages of this is to
+            produce code with high test coverage, leading to code that is easier
+            to maintain and extend.
+            <br></br>
+            <br></br>Our system makes use of a class named billboard which is
+            used to encapsulate all the information required to represent a
+            billboard object. Our team chose to implement this because it’s
+            repeated use across many classes involved with the Control Panel
+            Client, thus, getting this right would early in the development
+            would leverage off more risk in the future.
+            <br></br>
+            <br></br>To follow the TDD principles, we split the test writing and
+            fulfilment between myself and one of my team members. Test
+            fulfilment was was defined as having the test pass in the Junit
+            output window(testing utility available in intelliJ).#add in image
+            with junit tests passing Once fulfilled, they could be refactored
+            and re-tested to ensure the still provided the same functionality.
+            While the application grew during the development process, these
+            tests (and any other tests that have been written) would be
+            continually run to ensure this class was behaving as it was
+            intended.
+            <br></br>
+            <br></br>My experience using TDD was overwhelmingly positive for
+            several reasons. First, the process forces you to think of an entire
+            class and it's uses to near completion, before you have the chance
+            to write any methods. It acted as a blueprint for what the class
+            should do and the requirements were asserted and only met once all
+            tests were passed; once they did so, I could be sure that the
+            functionality was complete, meaning that additional work was making
+            the code better. Secondly, (and i'm aware this is hard to quantify)
+            it made my mind more clear for working on the problems at hand. It
+            was liberating know that as long as the Junit tests were passing, it
+            was highly likely that the related methods were working as they
+            should be. It enable me to concentrate more deeply by allowing me to
+            abstract myself away from exactly how each function worked. Finally,
+            TDD offered a new lens to gauge the current development speed. Since
+            the tests are written first, it's easier to track the progress of
+            method completion, assuming of course the project being worked on
+            requires a testing suite. Having the test written first makes sure
+            that at the end of each development cycle, each method has it's own
+            test(s). This is easier than estimating development speed first
+            because efforts may be wasted in writing a function that does not
+            fulfil it's requirements only after it's implementation.
             <br></br>
             <br></br>
             <h2>Requests, Reponses and Mocking</h2>
