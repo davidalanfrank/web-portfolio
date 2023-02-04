ed463d35db661f1e8a5f771273385e254669c522
davidalanfrank <davidfrankwebster@gmail.com>
2023-02-04 15:24:28 +1000

    wip

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,20 +5,21 @@ import "./about.scss";
 export default function About() {
   return (
     <div className="about" id="about">
-      Welcome to my website! 
-      <div>some welcome image or logo</div>
+     <h2 >Welcome to my website!</h2>
+      <div >some welcome image or logo</div>
       A bit about me; Monday - Friday I build and maintain digital products
       that help people move big things all across Australia. 
       <div>Truck logo</div>
 
-      A recent achievement; I graduated computer science! (sem2, 22).
-      <div>Truck logo</div>
-      Here are some project that i've completed in the pas
+      Recent news; <p>I graduated from computer science! (sem2, 22).</p>
+      <div>Graduation hat</div>
+      Right now (outside of work), my main project i'm working on building and writing content for
+      this portfolio and exercising.
+      <p>Here are some projects i've completed and written about. </p>
       <a href="/projects">
         <span>Projects</span>
       </a>
-      Outside of work i'm working on building and writing content for
-      this portfolio and exercising.
+
 
     </div>
   );
