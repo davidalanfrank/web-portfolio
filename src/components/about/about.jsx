09ef4d3bfd5dc8abec89981741f573e45e890348
davidalanfrank <davidfrankwebster@gmail.com>
2023-02-26 16:49:06 +1000

    wip

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -16,99 +16,14 @@ export default function About() {
   return (
     <div className="about" id="about">
       <div className="title">
-        {" "}
-        <br></br>
-        <h3>Welcome to my website!</h3>
-        <br></br>
+        <h1>Welcome to my website</h1>
+        <ul>
+          <li className="active">A bit about me</li>
+          <li>Recent New;</li>
+          <li>Currently;</li>
+        </ul>
       </div>
-
-      <Table striped bordered hover style={{ width: "90%" }}>
-        <tbody className="content-table">
-          <tr>
-            <td colSpan={1} className="text-td">
-              <span>
-                <span style={{ lineHeight: "2" }}>
-                  A bit about me;
-                  <br />
-                </span>
-                Mon- Fri I build and maintain digital products that help people
-                move big things all across Australia.
-              </span>
-            </td>
-            <td className="icon-td">
-              <div>
-                <img className="icon-style" src="assets/box-truck.png"></img>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <Table striped bordered hover style={{ width: "90%" }}>
-        <tbody className="remove-section-mobile">
-          <tr>
-            <td className="icon-td">
-              <div>
-                <img
-                  className="icon-style"
-                  src="assets/graduation-hat.png"
-                ></img>
-              </div>
-            </td>
-            <td colSpan={1} className="text-td right-align">
-              <div></div>
-              <span>
-                <span style={{ lineHeight: "2" }}>Recent news;</span>
-                <br />I graduated from computer science! (sem2, 22).
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <Table striped bordered hover style={{ width: "90%" }}>
-        <tbody>
-          <tr>
-            <td colSpan={1} className="text-td">
-              <span>
-                <span style={{ lineHeight: "2" }}>Right now;</span>
-                <br />
-                my main project i'm working on building and writing content for
-                this portfolio and exercising.
-              </span>
-            </td>
-            <td className="icon-td">
-              <div>
-                <img
-                  className="icon-style"
-                  src="assets/web-development.png"
-                ></img>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <br />
-      <br />
-      <br />
-      <Table striped bordered hover style={{ width: "90%" }}>
-        <tbody>
-          <tr>Here are some projects i've completed and written about.</tr>
-          <tr>
-            <button
-              onClick={goToProjects}
-              style={{
-                backgroundColor: "purple",
-                padding: "10px 20px",
-                borderRadius: "5px",
-                margin: "10px 0px",
-              }}
-              href="/projects"
-            >
-              {/* <Work className="icon" /> */}
-              <span className="button-style">See Projects</span>
-            </button>
-          </tr>
-        </tbody>
-      </Table>
+      <div className="container">firmwareLOIUY</div>
     </div>
   );
 }
